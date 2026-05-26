02953b4d0dbb961402cf01016be605d28ea77959
Sai Pradeep <saipradeep@Sais-MacBook-Air.local>
2026-05-26 15:11:48 +0530

    Fix Groq environment variable

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,7 +1,6 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    OPENAPI_API_KEY: str
     GROQ_API_KEY: str
     APP_NAME: str = "AstraRAG"
     APP_VERSION: str = "1.0.0"
